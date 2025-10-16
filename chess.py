dbddb319b3ef2fc0e90671cc2949abbab93a030c
SudevOP1 <sudevdahitule06@gmail.com>
2025-10-17 01:34:31 +0530

    added board drawing logic

--- chess.py
+++ chess.py
@@ -1,24 +1,62 @@
 import pygame as py
 from settings import *
 
-# py setup
-py.init()
-screen = py.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-clock = py.time.Clock()
-running = True
 
-while running:
-    for event in py.event.get():
-        if event.type == py.QUIT:
-            running = False
+def main():
 
-    # rendering game here
-    screen.fill("black")
-    mouse_x, mouse_y = py.mouse.get_pos()
-    py.draw.circle(screen, (255, 0, 0), (mouse_x, mouse_y), 20)
+    # pygame setup
+    py.init()
+    screen = py.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    clock = py.time.Clock()
+    running = True
 
-    py.display.flip()
-    clock.tick(GAME_FPS) # fps
+    while running:
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                running = False
 
-py.quit()
+        # rendering game here
+        screen.fill(BG_COLOR)
 
+        # drawing board
+        padding_x = (WINDOW_WIDTH - 8 * SQUARE_SIZE) / 2
+        padding_y = (WINDOW_HEIGHT - 8 * SQUARE_SIZE) / 2
+        for i in range(8):
+            for j in range(8):
+                # dark square
+                if (i + j) % 2 == 0:
+                    py.draw.rect(
+                        screen,
+                        DARK_COLOR,
+                        (
+                            padding_x + i * SQUARE_SIZE,
+                            padding_y + j * SQUARE_SIZE,
+                            SQUARE_SIZE,
+                            SQUARE_SIZE,
+                        ),
+                    )
+                # light square
+                else:
+                    py.draw.rect(
+                        screen,
+                        LIGHT_COLOR,
+                        (
+                            padding_x + i * SQUARE_SIZE,
+                            padding_y + j * SQUARE_SIZE,
+                            SQUARE_SIZE,
+                            SQUARE_SIZE,
+                        ),
+                    )
+
+        # red circle at mouse pos
+        mouse_x, mouse_y = py.mouse.get_pos()
+        py.draw.circle(screen, (255, 0, 0), (mouse_x, mouse_y), 20)
+
+        py.display.flip()
+        clock.tick(GAME_FPS)  # fps
+
+    py.quit()
+
+
+if __name__ == "__main__":
+    main()
