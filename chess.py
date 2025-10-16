b834c79803419b4242e640bcc9a50529351192a3
SudevOP1 <sudevdahitule06@gmail.com>
2025-10-17 01:35:58 +0530

    separated code in functions

--- chess.py
+++ chess.py
@@ -4,6 +4,38 @@ from settings import *
 
 def main():
 
+    def draw_board():
+        padding_x = (WINDOW_WIDTH - 8 * SQUARE_SIZE) / 2
+        padding_y = (WINDOW_HEIGHT - 8 * SQUARE_SIZE) / 2
+        for i in range(8):
+            for j in range(8):
+
+                # dark square
+                if (i + j) % 2 == 0:
+                    py.draw.rect(
+                        screen,
+                        DARK_COLOR,
+                        (
+                            padding_x + i * SQUARE_SIZE,
+                            padding_y + j * SQUARE_SIZE,
+                            SQUARE_SIZE,
+                            SQUARE_SIZE,
+                        ),
+                    )
+
+                # light square
+                else:
+                    py.draw.rect(
+                        screen,
+                        LIGHT_COLOR,
+                        (
+                            padding_x + i * SQUARE_SIZE,
+                            padding_y + j * SQUARE_SIZE,
+                            SQUARE_SIZE,
+                            SQUARE_SIZE,
+                        ),
+                    )
+
     # pygame setup
     py.init()
     screen = py.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
@@ -18,37 +50,9 @@ def main():
         # rendering game here
         screen.fill(BG_COLOR)
 
-        # drawing board
-        padding_x = (WINDOW_WIDTH - 8 * SQUARE_SIZE) / 2
-        padding_y = (WINDOW_HEIGHT - 8 * SQUARE_SIZE) / 2
-        for i in range(8):
-            for j in range(8):
-                # dark square
-                if (i + j) % 2 == 0:
-                    py.draw.rect(
-                        screen,
-                        DARK_COLOR,
-                        (
-                            padding_x + i * SQUARE_SIZE,
-                            padding_y + j * SQUARE_SIZE,
-                            SQUARE_SIZE,
-                            SQUARE_SIZE,
-                        ),
-                    )
-                # light square
-                else:
-                    py.draw.rect(
-                        screen,
-                        LIGHT_COLOR,
-                        (
-                            padding_x + i * SQUARE_SIZE,
-                            padding_y + j * SQUARE_SIZE,
-                            SQUARE_SIZE,
-                            SQUARE_SIZE,
-                        ),
-                    )
+        draw_board()
 
-        # red circle at mouse pos
+        # red circle at mouse pos (temporary)
         mouse_x, mouse_y = py.mouse.get_pos()
         py.draw.circle(screen, (255, 0, 0), (mouse_x, mouse_y), 20)
 
