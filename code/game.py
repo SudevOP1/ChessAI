5afbfa8cf4c7e1d14d6a1159f9e7fdd158abc8f7
SudevOP1 <sudevdahitule06@gmail.com>
2025-10-29 18:25:14 +0530

    minor change

--- code/game.py
+++ code/game.py
@@ -16,14 +16,14 @@ def input_player_option(color: str) -> Callable | None:
     set_printing_color("clear")
     _choice = input()
     while True:
-        if not (_choice.isdigit() and (1 <= int(_choice) <= len(PLAYER_OPTIONS))):
+        if not (_choice.isdigit() and (0 <= int(_choice) < len(PLAYER_OPTIONS))):
             set_printing_color("red")
             print(f"enter valid {color} player: ", end="")
             set_printing_color("clear")
             _choice = input()
             continue
 
-        _key = list(PLAYER_OPTIONS.keys())[int(_choice) - 1]
+        _key = list(PLAYER_OPTIONS.keys())[int(_choice)]
         return PLAYER_OPTIONS[_key]
 
 
@@ -32,7 +32,7 @@ def print_player_options():
     print("player options:")
     for _idx, _player_option in enumerate(PLAYER_OPTIONS.keys()):
         set_printing_color("clear")
-        print(_idx + 1, end="")
+        print(_idx, end="")
         set_printing_color("yellow")
         print(" for ", end="")
         set_printing_color("clear")
